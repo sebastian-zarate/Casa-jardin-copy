c23d9dc59f1c71addf4dc6d82cc6abdbb4758a68
Korosensee <francisco.girard@uap.edu.ar>
2024-08-08 17:03:29 -0300

    Comenzamos con el proyecto, muchachosss

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+function Login() {
+    return(
+        <main>
+
+            <div>
+
+                <img src="" alt="" />
+
+                <h2>Inicia Sesión</h2>
+
+                <div>
+                    <label htmlFor="">Email</label>
+                    <input type="text" />
+
+                    <label htmlFor="">Contraseña</label>
+                    <input type="password" />
+                </div>
+
+                <button>Iniciar Sesión</button>
+                <div>
+                    <span>No tienes cuenta?</span>
+                    <span>Registrarse</span>
+                </div>
+            </div>
+        </main>
+        
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+function Signup() {
+    return(
+        <main>
+
+            <div>
+
+                <img src="" alt="" />
+
+                <h2>Registrarte</h2>
+
+                <div>
+                    <label htmlFor="">Nombre</label>
+                    <input type="text" />
+
+                    <label htmlFor="">Apellido</label>
+                    <input type="text" />
+
+                    <label htmlFor="">Email</label>
+                    <input type="text" />
+
+                    <label htmlFor="">Contraseña</label>
+                    <input type="password" />
+
+                    <label htmlFor="">Confirmar Contraseña</label>
+                    <input type="password" />
+                </div>
+
+                <button>Iniciar Sesión</button>
+                <div>
+                    <span>Tienes cuenta?</span>
+                    <span>Iniciar Sesión</span>
+                </div>
+            </div>
+        </main>
+        
+    )
+}
