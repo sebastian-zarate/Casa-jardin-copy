f118f1d328f7f0fbc01f1baca679a850cb8add2e
Mateo <maldonado12net@gmail.com>
2024-09-06 17:22:47 -0300

    pagina profesionales y service

---
+++ src/services/profesional.ts
@@ -0,0 +1,66 @@
+'use server'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export type Profesional = {
+    id: number;
+    nombre: string;
+    apellido: string;
+    especialidad: string;
+    email: string;
+    telefono: number;
+    password: string;
+    direccionId: number;
+  }
+
+export async function getProfesionales() {
+  return await prisma.profesional.findMany();
+}
+
+export async function getProfesionalById(id: number) {
+  return await prisma.profesional.findUnique({
+    where: {
+      id,
+    },
+  });
+}
+
+export async function createProfesional(data: {
+    nombre: string;
+    apellido: string;
+    especialidad: string;
+    email: string;
+    telefono: number;
+    password: string;
+    direccionId: number;
+}) {
+  return await prisma.profesional.create({
+    data,
+  });
+}
+
+export async function updateProfesional(id: number, data: {
+    nombre?: string;
+    apellido?: string;
+    especialidad?: string;
+    email?: string;
+    telefono?: number;
+    password?: string;
+    direccionId?: number;
+}) {
+  return await prisma.profesional.update({
+    where: {
+      id,
+    },
+    data,
+  });
+}
+
+export async function deleteProfesional(id: number) {
+  return await prisma.profesional.delete({
+    where: {
+      id,
+    },
+  });
+}
