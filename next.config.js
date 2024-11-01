100b747b45e35f89ddbb26a3b1de40546ef717b4
Mateo <maldonado12net@gmail.com>
2024-11-01 18:04:48 -0300

    probando deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
     images: {
       domains: ['raw.githubusercontent.com'], // Agrega aquí el host correspondiente
     },
+    eslint: {
+      ignoreDuringBuilds: true, // Ignora errores de ESLint al hacer build
+    },
   };
