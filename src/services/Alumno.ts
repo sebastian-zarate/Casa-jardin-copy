c80d108b65a4b3b43ae7d05b803b8fdfa86455ed
Mateo <maldonado12net@gmail.com>
2024-11-15 18:09:09 -0300

    Version deployable

--- src/services/Alumno.ts
+++ src/services/Alumno.ts
@@ -29,7 +29,7 @@ export async function createAlumno(data: {
   apellido: string;
   email: string;
   password: string;
-  telefono: number;
+  telefono?: number;
   direccionId?: number;
   fechaNacimiento?: Date;
   mayoriaEdad?: boolean
