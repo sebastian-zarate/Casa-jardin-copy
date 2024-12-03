0ba1865244f48375d755846e9e365e4bed8c461e
AlanGoltz <alan.benner@uap.edu.ar>
2024-12-03 14:46:23 -0300

    Todo junto pa

--- src/app/usuario/principal/page.tsx
+++ src/app/usuario/principal/page.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import Navigate from "../../../components/alumno/navigate/page";
 import But_aside from "../../../components/but_aside/page";
 import { getCursosByIdAlumno } from "@/services/alumno_curso";
-
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { autorizarUser, fetchUserData } from "@/helpers/cookies";
